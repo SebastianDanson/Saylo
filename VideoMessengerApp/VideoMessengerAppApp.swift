aadcf947f945188e1bb3f7fbbd2f6e1a2402a449
Sebastian Danson <sebastian.danson@gmail.com>
2021-09-23 19:29:04 -0400

    Initial Commit

---
+++ VideoMessengerApp/VideoMessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoMessengerAppApp.swift
+//  VideoMessengerApp
+//
+//  Created by Student on 2021-09-23.
+//
+
+import SwiftUI
+
+@main
+struct VideoMessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
